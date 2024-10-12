f9b8ac6634b97d73f98c484bdc88fbd7321ebf1b
Jaskeerat Brar <jassibrar1999@gmail.com>
2024-10-11 22:41:02 -0700

    Update 413. Arithmetic Slices.java

--- 413. Arithmetic Slices.java
+++ 413. Arithmetic Slices.java
@@ -23,3 +23,31 @@ class Solution {
 
 // T(C): O(n)
 // S(C): O(1)
+
+/// Front iteration also
+
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+
+        int result = 0;
+        int curr = 0;
+        int n = nums.length;
+
+        for(int i = 2 ; i < n; i++){
+
+            if(nums[i-1] - nums[i-2] == nums[i] - nums[i-1]){
+                curr += 1;
+                result += curr;
+            }
+            else{
+                curr = 0;
+            }
+        }
+        
+        return result;
+    }
+}
+
+
+// T(C): O(n)
+// S(C): O(1)
